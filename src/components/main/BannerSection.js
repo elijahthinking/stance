63c2be12ab048dd3ee105fde00b84600f486f573
dahye0937 <dahye0937@gmail.com>
2025-12-23 14:47:15 +0900

    data

--- src/components/main/BannerSection.js
+++ src/components/main/BannerSection.js
@@ -1,7 +1,16 @@
+import "./BannerSection.scss"
+const images = [
+  "/images/banner01.png",
+  "/images/banner02.png",
+  "/images/banner03.png",
+  "/images/banner04.png",
+  "/images/banner05.png"
+];
+
 const BannerSection = () => {
   return (
     <section id="banner">
-      BannerSection
+      BannerSectiona
       </section>
   )
 }
